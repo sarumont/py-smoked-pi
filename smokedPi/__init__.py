87cfa2e28af9a4c89f4ad38557bbaaaf65facd84
Richard Kolkovich <richard@sigil.org>
2015-01-18 13:40:05 -0700

    add init

---
+++ smokedPi/__init__.py
@@ -0,0 +1,13 @@
+import sys
+import Adafruit_MAX1855.MAX31855 as MAX31855
+
+config = {
+        "CLK": 25,
+        "CS": 24,
+        "DO": 18
+        }
+
+if __name__ == "__main__":
+    print("Hello, world")
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
