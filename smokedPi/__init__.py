c0a083ef8a6442784ffa987da9fb300afb594078
Richard Kolkovich <richard@sigil.org>
2015-01-18 13:59:13 -0700

    Add example code

--- smokedPi/__init__.py
+++ smokedPi/__init__.py
@@ -8,6 +8,13 @@ config = {
         }
 
 if __name__ == "__main__":
-    print("Hello, world")
+    print("Hello, world %d %d %d" % (config["CLK"], config["CS"], config["DO"]))
+    sensor = MAX31855.MAX31855(config["CLK"], config["CS"], config["DO"])
+    while True:
+            temp = sensor.readTempC()
+            internal = sensor.readInternalC()
+            print 'Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp))
+            print '    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal))
+            time.sleep(1.0)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
